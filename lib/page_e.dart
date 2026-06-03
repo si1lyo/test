35896c3243f83385cce900e713748d77d8a57e26
REO-hama <izaruga5@gmail.com>
2026-06-03 19:53:54 +0900

    設定の項目を追加したよ

--- lib/page_e.dart
+++ lib/page_e.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class SettingPage extends StatelessWidget {
-  const SettingPage({super.key});
+class PageE extends StatelessWidget {
+  const PageE({super.key});
 
   @override
   Widget build(BuildContext context) {
